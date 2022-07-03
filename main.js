d798a0f5c6c78d78b6c1afc0ced5c7d771a2fa89
farshidev <65474063+farshidev@users.noreply.github.com>
2022-07-03 05:47:07 +0430

    compressing the images size

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+//changing the background randomly
 function changeBk() {
 
     const images = [
@@ -16,4 +16,36 @@ function changeBk() {
 
 }
 
-setInterval(changeBk, 3000)
+setInterval(changeBk, 3000);
+
+
+
+// adding the dragable scrolling on the .gallery section
+const slider = document.querySelector('.gallery');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    startX = e.pageX;
+    scrollLeft = slider.scrollLeft;
+
+})
+slider.addEventListener('mouseleave',() => {
+    isDown = false;
+})
+slider.addEventListener('mouseup',() => {
+    isDown = false;
+})
+slider.addEventListener('mousemove' ,(e) => {
+    if(!isDown) return; //stop the function from running
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 2; 
+    slider.scrollLeft = scrollLeft - walk;
+
+})
+
+
